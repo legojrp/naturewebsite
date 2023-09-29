1963dd0f19d33da7209fb87abe5d34890381e759
John Patch <legojrp@gmail.com>
2023-09-28 20:38:21 -0400

    This is a boilerplate for structure

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php 
+// Tell me when you want server - client access, and I (John) can insert the php to transfer that info
+?> 
