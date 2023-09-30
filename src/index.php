b9e4b52b0a8b5c0717e6c289790df1ad7326022e
John Patch <legojrp@gmail.com>
2023-09-29 21:24:23 -0400

    adde boilderplate

--- src/index.php
+++ src/index.php
@@ -6,10 +6,17 @@
     <title>Index</title>
 </head>
 <body>
-    
+    <nav class="">
+        <h3>Riverside Nature</h3>
+        <?php 
+            //add the admin or sign in
+        ?>
+    </nav>
+
 </body>
 </html>
 
 <?php 
 // Tell me when you want server - client access, and I (John) can insert the php to transfer that info
+echo "hey"
 ?> 
