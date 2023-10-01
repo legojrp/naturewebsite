da8d113955fbebb0bb953a3e323a02d4a34c46b6
John Patch <legojrp@gmail.com>
2023-09-30 20:57:35 -0400

    Update logger.php

--- src/backend/logger.php
+++ src/backend/logger.php
@@ -1,6 +1,6 @@
 <?php 
 function logMessage($level, $message) {
-    $logFile = 'log.log';
+    $logFile = '/src/backend/log.log';
 
     // Generate a timestamp
     $timestamp = date('Y-m-d H:i:s');
