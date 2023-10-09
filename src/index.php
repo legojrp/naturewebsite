aaf1e9505abfdac26ccf165319e3f7a24a23e5d0
John Patch <legojrp@gmail.com>
2023-10-09 18:12:06 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Project</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./app/styles/style.css">
+    <link rel="stylesheet" href="app/styles/styles.css">
 </head>
 <body>
     <?php
