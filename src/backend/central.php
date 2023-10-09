49f2cdbd395a4a6c5d2d89079899440966120de4
John Patch <legojrp@gmail.com>
2023-10-09 17:56:07 -0400

    Update central.php

--- src/backend/central.php
+++ src/backend/central.php
@@ -10,16 +10,15 @@ function createCard(){
     $db = new natureDB();
     foreach($db->displayMainPageNoFilter() as $row){
 
-        $output .= `<div class="col-md-6">
-                <div class="card mb-4">
-                    <img src="pics/{$row['description']}" class="card-img-top" alt="Picture of {$row["name"]}">
-                    <div class="card-body">
-                        <h5 class="card-title">{$row["name"]}</h5>
-                        <p class="card-text">{$row["description"]}</p>
-                    </div>
-                </div>
+        $output .= "<div class='col-md-6'>
+        <div class='card mb-4'>
+            <img src='pics/{$row['description']}' class='card-img-top' alt='Picture of {$row["name"]}'>
+            <div class='card-body'>
+                <h5 class='card-title'>{$row["name"]}</h5>
+                <p class='card-text'>{$row["description"]}</p>
             </div>
-            `;
+        </div>
+    </div>";
         
 
         
