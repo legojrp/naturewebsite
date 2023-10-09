277b9d2a84093e04c6a4baf0f823f451c32c04c6
John Patch <legojrp@gmail.com>
2023-10-09 17:40:32 -0400

    Update central.php

--- src/backend/central.php
+++ src/backend/central.php
@@ -8,17 +8,17 @@ require_once "dbconnect.php";
 function createCard(){
     $db = new natureDB();
     foreach($db->displayMainPageNoFilter() as $row){
-        // echo `<div class="col-md-6">
-        //         <div class="card mb-4">
-        //             <img src="https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg" class="card-img-top" alt="Event">
-        //             <div class="card-body">
-        //                 <h5 class="card-title">Event 2 Title</h5>
-        //                 <p class="card-text">Event 2 Description</p>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     `;
-        print_r($row);
+        echo `<div class="col-md-6">
+                <div class="card mb-4">
+                    <img src="pics/{$row['description']}" class="card-img-top" alt="Picture of {$row["name"]}">
+                    <div class="card-body">
+                        <h5 class="card-title">{$row["name"]}</h5>
+                        <p class="card-text">{$row["description"]}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        
     }
 
 
