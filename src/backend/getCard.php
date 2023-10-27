d10189e851e735c03e962d969cacb4ee8f5ae0ac
John Patch <legojrp@gmail.com>
2023-10-26 20:48:30 -0400

    Update getCard.php

--- src/backend/getCard.php
+++ src/backend/getCard.php
@@ -3,12 +3,12 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL); 
-    $Cid = $_POST["id"]; // Cardid
+    $Cid = $_POST["id"]; // Card id
     $sql = new natureDB();
     $row = $sql->getInfoFromId($Cid);
     echo $Cid;
     if ($row) {
-        echo json_encode(array("status"=> false,"msg"=> "Card Id doesn't exist, if the problem persists, email legojrp@gmail.com for assistance"));
+        echo json_encode(array("status"=> false,"msg"=> "Card Id: $Cid doesn't exist, if the problem persists, email legojrp@gmail.com for assistance"));
     }
     else {
         echo $row;
