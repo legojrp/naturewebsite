a41bac8a94aaae5a2c1de21fba020dab2624900f
John Patch <legojrp@gmail.com>
2023-10-28 14:57:45 -0400

    Update index.php

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php 
+        require_once "../backend/central.php";
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
