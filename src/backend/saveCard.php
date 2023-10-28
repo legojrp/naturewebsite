16d4e79a5da29fabc01679f62b7db80c0bb67fde
John Patch <legojrp@gmail.com>
2023-10-28 14:27:32 -0400

    Update saveCard.php

--- src/backend/saveCard.php
+++ src/backend/saveCard.php
@@ -1,5 +1,11 @@
 <?php 
     require_once "central.php";
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL); 
+
+    
     if (isset($_FILES["fileToUpload"])){
         $file = $_FILES["fileToUpload"];
         $targetDir = "../pics/";
