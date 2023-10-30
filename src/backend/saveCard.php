65a0d887469e80b52003cf6128a3c418f6b7d081
John Patch <legojrp@gmail.com>
2023-10-30 19:50:21 -0400

    Update saveCard.php

--- src/backend/saveCard.php
+++ src/backend/saveCard.php
@@ -1,9 +1,9 @@
 <?php 
     require_once "central.php";
 
-    // ini_set('display_errors', 1);
-    // ini_set('display_startup_errors', 1);
-    // error_reporting(E_ALL); 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL); 
 
     
     if (isset($_FILES["imageUpload"])){ // file upload not functional
